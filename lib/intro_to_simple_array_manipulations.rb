8b46a87e33c8aea9717ecdcb1816e7def415d63c
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-10 17:02:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,8 @@
 def using_push(colors_of_the_rainbow, next_color)
   next_color = "violet"
-  colors_of_the_rainbow.push(next_color)
+#  colors_of_the_rainbow.push(next_color)
   updated_array = colors_of_the_rainbow.push(next_color)
 end
-def using_unshift(bououghs_, new_neighborhood)
-  new_neighborhood
+def using_unshift(bouroughs_in_nyc, new_neighborhood)
+  new_neighborhood = "staten island"
+  
