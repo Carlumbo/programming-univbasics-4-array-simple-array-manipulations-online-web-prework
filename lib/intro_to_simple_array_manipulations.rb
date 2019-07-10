85ed5c512762902fc3dffb03cb52fb01bcf45769
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-10 17:13:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -20,6 +20,6 @@ def using_shift(my_favorite_cities)
   im_so_over_this_city= my_favorite_cities.shift
 end
 
-def def using_shift(my_favorite_cities)
+def def shift_with_args(my_favorite_cities)
   im_so_over_this_city= my_favorite_cities.shift(2)
 end
