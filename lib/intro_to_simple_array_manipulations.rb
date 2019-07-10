b3f31047470122f113e15888ec65e1177c7b21bc
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-10 17:02:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,5 @@
 def using_push(colors_of_the_rainbow, next_color)
   next_color = "violet"
-#  colors_of_the_rainbow.push(next_color)
   updated_array = colors_of_the_rainbow.push(next_color)
 end
 def using_unshift(bouroughs_in_nyc, new_neighborhood)
