a3a9f5628673dcbef6a63d082cea7aacfd3fa67d
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-10 16:58:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,4 @@
-def using_push(colors_in_the_rainbow, "violet")
+def using_push(colors_in_the_rainbow, next_color)
   next_color = "violet"
   colors_in_the_rainbow.push(next_color)
   updated_array = colors_of_the_rainbow.push(next_color)
