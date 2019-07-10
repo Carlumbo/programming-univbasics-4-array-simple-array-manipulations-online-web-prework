e819e93c924b6e0c6fb366ca32dc7949f8fafc3f
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-10 17:17:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -24,4 +24,6 @@ def shift_with_args(ice_cream_brands)
   brands_removed= ice_cream_brands.shift(2)
 end
 
-def using_concat 
+def using_concat(my_favorite_things, more_favs)
+  all_my_favs = my_favotie_things.concat(more_favs)
+end
