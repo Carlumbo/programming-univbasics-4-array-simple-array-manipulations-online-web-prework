ee26685573899721deebd08c233320b4d8008fc2
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-10 17:20:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -28,4 +28,5 @@ def using_concat(my_favorite_things, more_favs)
   all_my_favs = my_favorite_things.concat(more_favs)
 end
 
-def using_insert()
+def using_insert(list_of_programming_languages, another_language)
+  new_list = list_of_programming_languages.insert(another_language)
