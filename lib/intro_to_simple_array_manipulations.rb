b5e6fc42718ae260ad917eec3cd19ad9082c317b
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-10 17:11:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,7 @@ def using_push(colors_of_the_rainbow, next_color)
   next_color = "violet"
   updated_array = colors_of_the_rainbow.push(next_color)
 end
+
 def using_unshift(bouroughs_in_nyc, new_neighborhood)
   new_neighborhood = "Staten Island"
   updated_array = bouroughs_in_nyc.unshift(new_neighborhood)
@@ -10,6 +11,11 @@ end
 def using_pop(contenients)
   updated_array = contenients.pop
 end
+
 def pop_with_args(dog_breeds)
   small_dogs= dog_breeds.pop(2) 
+end
+
+def using_shift(array)
+  updated_array= array.shift
 end
